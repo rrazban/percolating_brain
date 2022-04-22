2c5ef4da5ba300e0bc9bcca8ab80792a4403ed5e
rrazban <rmr4442@gmail.com>
2022-04-22 08:47:25 -0400

    add dependencies

---
+++ install.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+# installing python libraries
+
+pip3 install matplotlib;
+pip3 install scipy;
+pip3 install pandas;
+pip3 install numpy;
+pip3 install networkx;
+pip3 install dipy;
+pip3 install nilearn;
