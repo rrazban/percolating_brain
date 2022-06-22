d89ff53a5ff1568034943223efdf364b662a9bf2
rrazban <rmr4442@gmail.com>
2022-06-22 09:56:44 -0400

    include all ukb files

--- analyze/phenotypes/ukb/parse_ukb.py
+++ analyze/phenotypes/ukb/parse_ukb.py
@@ -17,11 +17,13 @@ def parse_raw_phenotype_file():
     ages = []
     diabetes = []
     bipolars = []
+    genders = []
 
     age_code = '21003-2.0'  #age at dMRI scan
     diabetes_code = '2976-0.0'  #age at diabetes diagnosis
     bipolar_dep_code = '20126-0.0'  #categorical, indicates bipolar or depression severity
 #   alzheimers_code = '42020-0.0'   #do not currently have in my raw ukb phenotype file
+    gender_code = '31-0.0'
 
 
     alpha_file = '../../ukb.csv'
@@ -38,21 +40,23 @@ def parse_raw_phenotype_file():
             age = words[columns.index(age_code)]
             diabete = words[columns.index(diabetes_code)]
             bipolar = words[columns.index(bipolar_dep_code)]
+            gender = words[columns.index(gender_code)]
         
             if int(eid) in dmris:
                 eids.append(eid)
                 ages.append(age)
                 diabetes.append(diabete)
                 bipolars.append(bipolar)
-    return eids, ages, diabetes, bipolars
+                genders.append(gender)
+    return eids, ages, diabetes, bipolars, genders
 
 
 
 if __name__ == '__main__':
-    eids, ages, diabetes, bipolars = parse_raw_phenotype_file()
+    eids, ages, diabetes, bipolars, genders = parse_raw_phenotype_file()
 
     print('Number of individuals parsed: {0}'.format( len(eids)))
 
-    df = pd.DataFrame(list(zip(*[eids, ages, diabetes, bipolars])))
-    df.columns = ['id', 'age', 'diabetes', 'bipolar/depression']
+    df = pd.DataFrame(list(zip(*[eids, ages, diabetes, bipolars, genders])))
+    df.columns = ['id', 'age', 'diabetes', 'bipolar/depression', 'gender']
     df.to_csv('phenotypes.csv', index=False)
