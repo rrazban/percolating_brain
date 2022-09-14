a0a6d458a33dcb8e95f1da69c2618733c6815b94
rrazban <rmr4442@gmail.com>
2022-09-14 13:04:34 -0400

    reorganize

--- simulation/standard_graphs/visualize/random_graph_k1.py
+++ simulation/standard_graphs/visualize/random_graph_k1.py
@@ -7,18 +7,28 @@ at an average degree of 1.
 
 import matplotlib.pyplot as plt
 import networkx as nx
+import numpy as np
 
 layout = nx.spring_layout
 
 
-n = 64  #corresponds to Harvard-Oxford atlas
-p_giant = 1.0 / (n )    #corresponds to k=1
+#n = 64  #corresponds to Harvard-Oxford atlas
+#p_giant = 1.0 / (n )    #corresponds to k=1
+
+n = 100
+p_giant = 50.0 / (n )    #corresponds to k=1
 
 
 for p in [p_giant]:
     plt.title(f"Random Graph Simulation", fontsize=16)
 
     G = nx.binomial_graph(n, p)
+    if True: 
+            print('average degree: {0:.2f}'.format(np.mean([deg for (node, deg) in G.degree()])))
+            print('average clustering coefficient: {0:.2f}'.format(nx.average_clustering(G)))
+            print('average shortest path length: {0}'.format(nx.average_shortest_path_length(G)))
+
+
     pos = layout(G)
     G.remove_nodes_from(list(nx.isolates(G)))	#remove orphans from visualization
     nx.draw(G, pos, with_labels=False, node_size=20)
